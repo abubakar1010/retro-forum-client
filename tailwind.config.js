31afa54f1ac012cf2c9d4079ab6910e0c8ab9e46
Abubakar1010 <abubakar850772@gmail.com>
2024-03-02 20:55:53 +0600

    project initialized with basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+            mulish: ["Mulish", 'sans-serif'],
+          },
+      },
+    },
+    plugins: [],
+  }
